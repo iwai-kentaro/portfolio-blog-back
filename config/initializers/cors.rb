738ea6e635478b0c3f14a46c1d7c861612006784
kentaro iwai <iwai.kentaro@wiserock.jp>
2025-01-26 06:16:08 +0900

    CORSで調整

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://portfolio-blog-front.vercel.app/", "http://localhost:5173"
+    origins ENV["APP_ORIGINS"] || "http://localhost:5173"
     resource "*",
              headers: :any,
              methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
