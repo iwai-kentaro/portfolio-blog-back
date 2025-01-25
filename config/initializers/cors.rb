3d84879a29a2c9efd045edd3e321778618ad0f66
kentaro iwai <iwai.kentaro@wiserock.jp>
2025-01-25 23:14:40 +0900

    更新

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do # http://localhost:5173 は vue が起動するポート
-    origins "http://localhost:3000", "http://localhost:5173"
-
-    resource "*",
-      headers: :any,
-      methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
+  allow do
+    origins ENV.fetch('CORS_ORIGINS', '*')
+    resource '*',
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
