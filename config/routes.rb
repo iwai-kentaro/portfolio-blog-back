a9c7cfff0f97c811a036169d171a13a47bcbb5b6
kentaro iwai <iwai.kentaro@wiserock.jp>
2025-01-25 17:06:51 +0900

    更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      root  'posts#index'  #この1行を追加
+
       resources :posts, only: [:index, :create, :destroy, :update, :show, :edit] 
     end
   end
