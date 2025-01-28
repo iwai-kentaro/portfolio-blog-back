0383d80b0d347adfd1ed73503d28b4da946bd5ed
kentaro iwai <iwai.kentaro@wiserock.jp>
2025-01-29 05:18:22 +0900

    userのバリデーション削除と追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :posts
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6 }
 
   private
 
